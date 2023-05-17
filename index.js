0705645090bf599bd1bb0e56e77c56af1692d0a0
hifzazunair7 <hifzazunair7@gmail.com>
2023-05-17 13:39:16 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+var marks = + prompt("enter you marks");
+if (marks <=100){
+    document.write("grade is a");
+}
+else if (marks <=80){
+document.write("grade is b");
+}
+else if (marks <=60){
+    document.write("grade is c");
+}
+else if (marks <=40){
+    document.write("grade is d");
+}
+else{
+    document.write("you are fail");
+}
+
